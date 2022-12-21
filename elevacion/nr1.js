fa7708548a514510c9e45106df64754f78ec17ca
FranciscoCaba <francorv2@fpuna.edu.py>
2022-12-21 12:04:58 -0300

    Analisis del nr1 hecho

---
+++ elevacion/nr1.js
@@ -0,0 +1,17 @@
+//CODIGO DADO
+console.log(hello);                                   
+var hello = 'world';                                 
+
+//LUEGO DE LA ELEVACION
+/**
+ * var hello;
+ * console.log(hello); //imprime undefined
+ * hello = 'world';
+ */
+
+//CON ES6
+/**
+ * console.log(hello); //tira un ReferenceError
+ * let hello = 'world';
+ * 
+ */
