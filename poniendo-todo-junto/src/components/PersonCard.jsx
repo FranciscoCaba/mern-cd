4360470134ba22e480242a19ce99dc8a17de267c
FranciscoCaba <francorv2@fpuna.edu.py>
2023-01-07 17:10:39 -0300

    Funcionalidad de botón añadida

--- poniendo-todo-junto/src/components/PersonCard.jsx
+++ poniendo-todo-junto/src/components/PersonCard.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        // const { firstName, lastName, age, hairColor } = this.props;
+        this.state = {
+            firstName: this.props.firstName,
+            lastName: this.props.lastName,
+            age: this.props.age,
+            hairColor: this.props.hairColor
+        }
+    }
+    updateAge = ()=>{
+        this.setState((state)=>({ age: state.age/1+1 }));
+    }
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
         return (
             <div>
-                <h2>{lastName}, {firstName}</h2>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
+                <h2>{this.state.lastName}, {this.state.firstName}</h2>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.state.hairColor}</p>
                 <div className="d-grid gap-2">
-                    <button type="button" className="btn btn-secondary btn-sm">Birthday Button for {firstName} {lastName}</button>
+                    <button onClick={this.updateAge} className="btn btn-secondary btn-sm">Birthday Button for {this.state.firstName} {this.state.lastName}</button>
                 </div>
             </div>
         )
