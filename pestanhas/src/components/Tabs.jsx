90e5fe6efced457b167e09415c382b21ff424245
FranciscoCaba <francorv2@fpuna.edu.py>
2023-01-12 21:39:33 -0300

    Animaciones añadidas

--- pestanhas/src/components/Tabs.jsx
+++ pestanhas/src/components/Tabs.jsx
@@ -1,9 +1,33 @@
 import React, { useState } from 'react'
 
 function Tabs(props) {
-    const [activeButton, setActiveButton] = useState()
+    const [activeButton, setActiveButton] = useState({})
+    function changeButtonColor(boton){
+        if(activeButton.etiqueta!==undefined){
+            activeButton.activo.classList.remove("colored")
+            boton.classList.add("colored")
+            setActiveButton({...activeButton, activo: boton})
+        }else{
+            boton.classList.add("colored")
+        }
+    }
+    function addAnimation(){
+        const elemento = document.querySelector("textarea");
+        const agrandar = [
+            { transform: 'translateY(100px) scale(0)' },
+            { transform: 'translateY(0px) scale(1)' }
+        ];
+        
+        const tiempoAgrandar = {
+            duration: 500,
+            iterations: 1,
+        }
+        elemento.animate(agrandar, tiempoAgrandar);
+    }
     function handleOnClick(e, etiqueta){
-        setActiveButton(etiqueta)
+        changeButtonColor(e.target)
+        setActiveButton({etiqueta,activo: e.target})
+        addAnimation()
     }
     return (
         <>
@@ -16,7 +40,7 @@ function Tabs(props) {
             </div>
             <textarea
                 value={
-                    props.array.filter(value => value.etiqueta===activeButton).map(value=>value.contenido)
+                    props.array.filter(value => value.etiqueta===activeButton.etiqueta).map(value=>value.contenido)
                 }
                 disabled rows="6" cols="30" style={{resize: 'none'}}/>
         </>
