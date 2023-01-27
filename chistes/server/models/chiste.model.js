ebd4d75fa5fe757dd3135532ea57708ab6388f41
FranciscoCaba <francorv2@fpuna.edu.py>
2023-01-27 19:06:08 -0300

    Validaciones agregadas

--- chistes/server/models/chiste.model.js
+++ chistes/server/models/chiste.model.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 
 const ChisteSchema = new mongoose.Schema({
-    setup: String,
-    punchline: String
+    setup: {
+        type: String,
+        minLength: [10, "El setup del chiste debe ser de al menos 10 caracteres"]
+    },
+    punchline: {
+        type: String,
+        minLength: [3, "El punchline del chiste debe ser de al menos 3 caracteres"]
+    }
 })
 
 const Chiste = mongoose.model("Chiste",ChisteSchema)
