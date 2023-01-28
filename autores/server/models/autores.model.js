bd50ce7c76180c9cad2cf1b1a1fe8d97347b38b2
FranciscoCaba <francorv2@fpuna.edu.py>
2023-01-28 20:18:46 -0300

    Campo LastName eliminado

--- autores/server/models/autores.model.js
+++ autores/server/models/autores.model.js
@@ -5,11 +5,6 @@ const AutorSchema = new mongoose.Schema({
         type: String,
         required: [true, "The first name is required"],
         minlength: [3, "The first name must be at least 3 characters long"]
-    },
-    lastName: {
-        type: String,
-        required: [true, "The last name is required"],
-        minlength: [3, "The last name must be at least 3 characters long"]
     }
 },{ timestamps: true })
 
