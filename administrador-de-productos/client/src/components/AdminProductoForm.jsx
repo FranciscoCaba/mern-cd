bbacccf306cb2eddda625a48e68c7d38a17884b2
FranciscoCaba <francorv2@fpuna.edu.py>
2023-01-27 23:10:03 -0300

    Formulario frontend implementado para postear

--- administrador-de-productos/client/src/components/AdminProductoForm.jsx
+++ administrador-de-productos/client/src/components/AdminProductoForm.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const AdminProductoForm = () => {
+    const [title, setTitle] = useState("")
+    const [price, setPrice] = useState(0)
+    const [description, setDescription] = useState("")
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
+        axios.post("http://localhost:8000/api/producto/new",{
+            title,
+            price,
+            description
+        })
+            .then( res => console.log(res.data))
+            .catch( err => console.log(err))
+    }
+
     return (
-        <div>AdminProductoForm</div>
+        <div>
+            <h1>Product Manager</h1>
+            <form onSubmit={onSubmitHandler}>
+                <p>
+                    <label>Title:</label>
+                    <input type="text" onChange={ e => setTitle(e.target.value) }/>
+                </p>
+                <p>
+                    <label>Price:</label>
+                    <input type="number" onChange={ e => setPrice(e.target.value) }/>
+                </p>
+                <p>
+                    <label>Description:</label>
+                    <input type="text" onChange={ e => setDescription(e.target.value) }/>
+                </p>
+                <button type='submit'>Create</button>
+            </form>
+        </div>
     )
 }
 
