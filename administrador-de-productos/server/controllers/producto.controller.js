678e3ef304adf24a0896dca79e20e3d52ab9dafe
FranciscoCaba <francorv2@fpuna.edu.py>
2023-01-27 22:57:27 -0300

    Funcion de agregar producto creada en backend

--- administrador-de-productos/server/controllers/producto.controller.js
+++ administrador-de-productos/server/controllers/producto.controller.js
@@ -1,5 +1,14 @@
 const Producto = require('../models/producto.model')
 
 module.exports = {
-    
+    addProduct: (req,res) => {
+        const { title, price, description } = req.body
+        Producto.create({
+            title,
+            price,
+            description
+        })
+            .then( newProduct => res.json(newProduct) )
+            .catch( err => res.json({message: "Ha ocurrido un error.", error: err}))
+    }
 }
--- administrador-de-productos/server/routes/producto.routes.js
+++ administrador-de-productos/server/routes/producto.routes.js
@@ -1,5 +1,5 @@
 const ProductoController = require('../controllers/producto.controller')
 
 module.exports = app => {
-    
+    app.post("/api/producto/new", ProductoController.addProduct)
 }
